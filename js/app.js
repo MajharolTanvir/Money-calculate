50fba675962eb1833da5e36e74d6d5d790bf7e83
Majharul tanvir <mdtanvir6070@gmail.com>
2022-02-19 19:10:32 +0600

    total expenses

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
-function calculateValue(){
- const foodInput = document.getElementById('food-price');
- const foodPrice = parseFloat(foodInput.value);
- const rentInput = document.getElementById('rent-price');
- const rentPrice = parseFloat(rentInput.value);
- const clothesInput = document.getElementById('clothes-price');
- const clothesPrice = parseFloat(clothesInput.value);
- const totalExpences = foodPrice + rentPrice + clothesPrice;
- return totalExpences;
+function calculateValue() {
+	// total expence money
+	const foodInput = document.getElementById('food-price');
+	const foodPrice = parseFloat(foodInput.value);
+	const rentInput = document.getElementById('rent-price');
+	const rentPrice = parseFloat(rentInput.value);
+	const clothesInput = document.getElementById('clothes-price');
+	const clothesPrice = parseFloat(clothesInput.value);
+	const totalExpencesmoney = foodPrice + rentPrice + clothesPrice;
+	// total expenses show
+	const expensesValue = document.getElementById ('total-expenses')
+	expensesValue.innerText = totalExpencesmoney;
+	const expensesText = parseFloat(expensesValue.innerText);
+	// Balance show
+
+	return expensesText;
+	
 
 
 }
-document.getElementById('Calc-btn').addEventListener('click', function(){
-    calculateValue();
+
+document.getElementById('Calc-btn').addEventListener('click', function () {
+	calculateValue();
 })
