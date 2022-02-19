3f6eebec9018e10e033c1d8ced6ec28fa0349be3
Majharul tanvir <mdtanvir6070@gmail.com>
2022-02-19 19:39:21 +0600

    first calculation

--- js/app.js
+++ js/app.js
@@ -7,16 +7,18 @@ function calculateValue() {
 	const clothesInput = document.getElementById('clothes-price');
 	const clothesPrice = parseFloat(clothesInput.value);
 	const totalExpencesmoney = foodPrice + rentPrice + clothesPrice;
+	// Income show
+	const incomeInput = document.getElementById('income-value')
+	const incomeFiled = parseFloat(incomeInput.value);
 	// total expenses show
 	const expensesValue = document.getElementById ('total-expenses')
 	expensesValue.innerText = totalExpencesmoney;
 	const expensesText = parseFloat(expensesValue.innerText);
 	// Balance show
-
-	return expensesText;
-	
-
-
+	const balanceInput = document.getElementById('balance-amount');
+	balanceInput.innerText = expensesText - incomeFiled;
+	const balanceText = parseFloat(balanceInput.innerText);
+	return balanceText;
 }
 
 document.getElementById('Calc-btn').addEventListener('click', function () {
